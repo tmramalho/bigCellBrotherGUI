506fa7355128e1a9dd690bc78b4e4eae4ec8f2cf
tiago <tiago@tiago-VirtualBox>
2012-09-12 17:11:46 +0200

    initial video support

--- bigCellBrother/main.cpp
+++ bigCellBrother/main.cpp
@@ -21,37 +21,56 @@ void thresh_callback(int t, void* c) {
 int main(int argc, char *argv[]) {
 	srand(time(NULL));
 	ImageSegmentor is;
+	//cv::Mat bfOriginalImage = cv::imread("data/cells_bf.png");
 
-	cv::Mat bfOriginalImage = cv::imread("data/cells_bf.png");
+	cv::VideoCapture capture("/media/sf_tiago/Downloads/ImageStacks_Brightfield_Fluorescence/compressed_10nm_wf_BF.avi");
+	if (!capture.isOpened()) {
+		std::cerr << "blooo" << std::endl;
+		return 1;
+	}
 
-	cv::Mat boostedImage = ImageProcessor::simplifyImage(bfOriginalImage, 3, 100, true);
-	cv::Mat improvImage = ImageProcessor::simplifyImage(bfOriginalImage, 5, 100);
-	improvImage = ImageProcessor::invertImage(improvImage);
+	//double rate= capture.get(CV_CAP_PROP_FPS);
+	cv::Mat frame; // current video frame
+	cv::namedWindow("Extracted Frame");
 
-	cv::Mat targetBlobs = ImageProcessor::adaptiveThreshold(improvImage, -4, 35, false);
-
-	cv::Mat backgroundMask = ImageProcessor::floodBackground(improvImage, 1105, 86, 3);
-	cv::bitwise_or(backgroundMask, ImageProcessor::invertImage(targetBlobs), backgroundMask);
-	backgroundMask = ImageProcessor::applyMorphologyOp(backgroundMask, cv::MORPH_CLOSE, 7);
-	backgroundMask = ImageProcessor::erode(backgroundMask, 5);
-
-	cv::subtract(targetBlobs, backgroundMask, targetBlobs);
-
-	cv::Mat distTrans = ImageProcessor::distanceTransform(targetBlobs);
-	distTrans = ImageProcessor::threshold(distTrans, 80, false);
-	distTrans = ImageProcessor::applyMorphologyOp(distTrans, cv::MORPH_OPEN, 3);
-
-	markersCont mcBfNice = is.makeNiceMarkers(distTrans, 50, 60, 12, 30, 40);
-	cv::Mat wsMarkersBfNice = is.drawMarkers(mcBfNice);
-	cv::imshow("markersBf2", wsMarkersBfNice);
-
-	cv::Mat wsMarkersFinal = is.addBackgroundMask(mcBfNice.markers, backgroundMask);
-
-	cv::Mat watershed = is.watershed(improvImage, wsMarkersFinal);
-
-	while(true) {
+	// for all frames in video
+	while (true) {
 		char c = cv::waitKey(0);
 		if (c == 27) break;
+		if (c != 0) {
+			if (!capture.read(frame)) // read next frame if any
+				break;
+
+			cv::imshow("Extracted Frame",frame);
+
+			cv::Mat improvImage = ImageProcessor::simplifyImage(frame, 5, 100);
+			improvImage = ImageProcessor::invertImage(improvImage);
+
+			cv::Mat targetBlobs = ImageProcessor::adaptiveThreshold(improvImage, -4, 35, false);
+
+			cv::Mat backgroundMask = ImageProcessor::floodBackground(improvImage, 1105, 86, 3);
+			cv::bitwise_or(backgroundMask, ImageProcessor::invertImage(targetBlobs), backgroundMask);
+			backgroundMask = ImageProcessor::applyMorphologyOp(backgroundMask, cv::MORPH_CLOSE, 7);
+			backgroundMask = ImageProcessor::erode(backgroundMask, 5);
+
+			cv::subtract(targetBlobs, backgroundMask, targetBlobs);
+
+			cv::Mat distTrans = ImageProcessor::distanceTransform(targetBlobs);
+			distTrans = ImageProcessor::threshold(distTrans, 80, false);
+			distTrans = ImageProcessor::applyMorphologyOp(distTrans, cv::MORPH_OPEN, 3);
+
+			markersCont mcBfNice = is.makeNiceMarkers(distTrans, 50, 60, 12, 30, 40);
+			cv::Mat wsMarkersBfNice = is.drawMarkers(mcBfNice);
+			cv::imshow("markersBf2", wsMarkersBfNice);
+
+			cv::Mat wsMarkersFinal = is.addBackgroundMask(mcBfNice.markers, backgroundMask);
+
+			cv::Mat watershed = is.watershed(improvImage, wsMarkersFinal);
+
+		}
 	}
+
+	capture.release();
+
 	return 0;
 }
